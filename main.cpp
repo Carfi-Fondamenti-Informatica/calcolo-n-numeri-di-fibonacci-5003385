56023c02f2ff6ed65df68ca1bc8a8b72b7c97c5c
5003385 <91500260+5003385@users.noreply.github.com>
2021-11-11 20:36:24 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,27 @@
 using namespace std;
 
 int main() {
+    int n;
+    int fibon=1;
+    int fib=0;
+    cin >> n;
+
+    if (n>=2){
+
+        cout << "1" << endl;
+
+        for (int i=1;i<n; i++){
+
+            fibon=fibon+fib;
+            fib=fibon-fib;
+
+            cout << fibon <<endl;
+
+        }
+
+    } else {
+        cout << "errore" << endl;
+    }
+
    return 0;
 }
